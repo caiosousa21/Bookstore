3ca27e6af7b158b65512ec651cde2cdb56199286
caiosousa21 <caiofmsousa@gmail.com>
2018-12-19 14:33:35 -0200

    iniciando testes, teste unitário realizado na função buscarLista()

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import Barra from './Barra';
 import Elemento from './Elemento';
 import './index.css'
+import {buscarLista} from './buscarLista'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     let lista = [];
-    lista = this.buscarLista();
+    lista = buscarLista();
     this.state = {
       itemsCarrinho: [],
       listaListas: [lista[0].a, lista[1].a, lista[2].a, lista[3].a],
@@ -77,40 +78,7 @@ class App extends React.Component {
     });
   }
 
-  buscarLista = () => {
-    // if(!this.EstadoLocal){
-    let url = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=7a7846ce976e4df6ae218249bfb3a631&list=';
-    function Get(urlLista) {
-      var Httpreq = new XMLHttpRequest();
-      Httpreq.open('GET', urlLista, false);
-      Httpreq.send(null);
-      return Httpreq.responseText;
-    }
-    let ListaListas = JSON.parse(Get(url));
-    let Listas = [];
-    for (let i = 0; i < 4; i++) {
-      let url = 'https://api.nytimes.com/svc/books/v3/lists.json?api-key=7a7846ce976e4df6ae218249bfb3a631&list=' + ListaListas.results[i].list_name_encoded;
-      function Get(urlLista) {
-        var Httpreq = new XMLHttpRequest();
-        Httpreq.open('GET', urlLista, false);
-        Httpreq.send(null);
-        return Httpreq.responseText;
-      }
-      let ListaLivros = JSON.parse(Get(url));
-      let ListaLivrosa = [];
-      for (let i = 0; i < ListaLivros.results.length; i++) {
-        ListaLivrosa[i] = {
-          titulo: ListaLivros.results[i].book_details[0].title,
-          autor: ListaLivros.results[i].book_details[0].author,
-          editora: ListaLivros.results[i].book_details[0].publisher,
-          data: ListaLivros.results[i].published_date
-        };
-      }
-      Listas[i] = { a: ListaListas.results[i].list_name, b: ListaLivrosa };
-    }
-    return (Listas)
-    // }
-  }
+  
 
   render() {
     return (
