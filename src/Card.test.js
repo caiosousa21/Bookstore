2cc8840d6a5693d60f680b1d2e93c4837dae2627
caiosousa21 <caiofmsousa@gmail.com>
2018-12-20 09:20:22 -0200

    Testando se componente react Card está sendo renderizado em tela, e adicionada config --verbose ao script de test

---
+++ src/Card.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card';
+import Enzyme, {shallow, mount} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()})
+
+describe('Card', ()=>{
+    const livro={
+        titulo:undefined, 
+        autor: undefined, 
+        editora:undefined,
+        data:undefined
+    }
+    test('renderizando Card',()=>{
+        const wrapper = shallow(<Card livro={livro}/>);
+        expect(wrapper.exists()).toBe(true);
+    })
+})
